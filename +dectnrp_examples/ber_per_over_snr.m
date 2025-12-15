b267d2888e9be7a7270ffd4cc0e13da0c9da3b35
Jon Montalban Sanchez <jon.montalban@ehu.eus>
2025-12-15 17:48:26 +0100

    New AWGN simulations
    The proposed Interpolation is not working very good, so I have create an ideal channel for AWGN estimation. The method should be updated for using any kind of channel.

--- +dectnrp_examples/ber_per_over_snr.m
+++ +dectnrp_examples/ber_per_over_snr.m
@@ -24,13 +24,13 @@ function [] = ber_per_over_snr()
     harq_retransmissions = 0;
     
     % simulation range for link level simulation
-    snr_db_init=[-3, 0, 3, 5,9];
+    snr_db_init=[-2, -1, 2, 4,8];
     snr_db_end=snr_db_init+5;
     snr_db_step=0.25;
     snr_db = snr_db_init(1):snr_db_step:snr_db_end(1);              % First test vector for defining the variables.
 
     % Packets per mcs and snr. Increase this number to get smoother curves.
-    n_packets_per_snr = 20000;
+    n_packets_per_snr = 25000;
     
     % result container for PCC
     n_bits_PCC_sent = zeros(numel(mcs), numel(snr_db(1,:)));        % BER uncoded
@@ -91,7 +91,7 @@ function [] = ber_per_over_snr()
         n_packets_PDC_error_row = zeros(1, numel(snr_db));
     
         %for i=1:numel(snr_db)
-        parfor i=1:numel(snr_db)
+        for i=1:numel(snr_db)
             
             warning('off');
             
